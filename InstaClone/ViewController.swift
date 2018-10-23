d6e23417503743578a18e88d91b04a5622814f45
Arslan <saeed.arslan07@gmail.com>
2018-10-23 17:20:42 -0400

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Arslan Saeed on 10/23/18.
+//  Copyright © 2018 Arslan Saeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
